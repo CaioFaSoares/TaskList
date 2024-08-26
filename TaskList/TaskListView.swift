f875baad5e04526afd61bd1cdf5a236842228685
Caio <caiofasoares@gmail.com>
2024-08-26 14:59:55 -0300

    Aula 2 -  Chkpt 1 - Ponto de partida

--- TaskList/TaskListView.swift
+++ TaskList/TaskListView.swift
@@ -16,17 +16,22 @@ struct Tarefa: Identifiable {
 
 struct TaskListView: View {
     
-    @State var tarefas: [Tarefa] = [Tarefa(), Tarefa()]
+    @State var tarefas: [Tarefa] = [
+        Tarefa(nome: "Comprar pão", date: Date.now),
+        Tarefa(nome: "Jogar Elden Ring", date: Date.now),
+        Tarefa(nome: "Resolver exercício de matemática", date: Date.now),
+        Tarefa(nome: "Estudar Swift", date: Date.now),
+        Tarefa(nome: "Fazer exercício físico", date: Date.now),
+        Tarefa(nome: "Ler um livro", date: Date.now),
+        Tarefa(nome: "Enviar e-mails", date: Date.now),
+        Tarefa(nome: "Limpar a casa", date: Date.now),
+        Tarefa(nome: "Organizar documentos", date: Date.now),
+        Tarefa(nome: "Preparar almoço", date: Date.now)
+    ]
     
     var body: some View {
-        NavigationStack {
-            List(tarefas) { tarefa in
-                NavigationLink {
-                    TaskDetailView()
-                } label: {
-                    Text("Navegar")
-                }
-            }
+        List(tarefas) { tarefa in
+            Text(tarefa.nome)
         }
     }
 }
