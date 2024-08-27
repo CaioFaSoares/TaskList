6a0dc52cabd03c5135f0eec4bfbbf3ac071a1c79
Caio <caiofasoares@gmail.com>
2024-08-27 10:14:16 -0300

    Checkpoint - Botões de Debug

--- TaskList/TaskListView.swift
+++ TaskList/TaskListView.swift
@@ -7,31 +7,63 @@
 
 import SwiftUI
 
-struct Tarefa: Identifiable {
-    let id = UUID()
-    var nome = ""
-    var date = Date.now
-    var pronta = false
-}
-
 struct TaskListView: View {
     
     @State var tarefas: [Tarefa] = [
-        Tarefa(nome: "Comprar pão", date: Date.now),
-        Tarefa(nome: "Jogar Elden Ring", date: Date.now),
-        Tarefa(nome: "Resolver exercício de matemática", date: Date.now),
-        Tarefa(nome: "Estudar Swift", date: Date.now),
-        Tarefa(nome: "Fazer exercício físico", date: Date.now),
-        Tarefa(nome: "Ler um livro", date: Date.now),
-        Tarefa(nome: "Enviar e-mails", date: Date.now),
-        Tarefa(nome: "Limpar a casa", date: Date.now),
-        Tarefa(nome: "Organizar documentos", date: Date.now),
-        Tarefa(nome: "Preparar almoço", date: Date.now)
+        Tarefa(nome: "Comprar pão"),
+        Tarefa(nome: "Jogar Elden Ring"),
+        Tarefa(nome: "Resolver exercício de matemática"),
+        Tarefa(nome: "Estudar Swift"),
+        Tarefa(nome: "Fazer exercício físico"),
+        Tarefa(nome: "Ler um livro"),
+        Tarefa(nome: "Enviar e-mails"),
+        Tarefa(nome: "Limpar a casa"),
+        Tarefa(nome: "Organizar documentos"),
+        Tarefa(nome: "Preparar almoço")
     ]
     
+    @State var adicionandoTarefa = false
+    
+    @State var nomeNovaTarefa = ""
+    
     var body: some View {
-        List(tarefas) { tarefa in
-            Text(tarefa.nome)
+        NavigationStack {
+            List {
+                ForEach(tarefas, id: \.self) { tarefa in
+                    NavigationLink {
+                        TaskDetailView()
+                    } label: {
+                        Text(tarefa.nome)
+                    }
+                }.onDelete { index in
+                    tarefas.remove(atOffsets: index)
+                }
+            }
+            .navigationTitle("Tarefas")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                Button {
+                    adicionandoTarefa = true
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+            .sheet(isPresented: $adicionandoTarefa) {
+                TextField("Nome da tarefa", text: $nomeNovaTarefa)
+                    .textFieldStyle(.roundedBorder)
+                    .padding(.all)
+                Button {
+                    if nomeNovaTarefa.isEmpty == false {
+                        tarefas.append(Tarefa(nome: nomeNovaTarefa))
+                        nomeNovaTarefa = ""
+                        print(tarefas.count)
+                        adicionandoTarefa = false
+                    }
+                } label: {
+                    Text("Adicionar")
+                }
+
+            }
         }
     }
 }
